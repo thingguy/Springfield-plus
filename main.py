7c920f6b83e29ff3920eeff7314aa5b4539f20fb
Matteo Gebb <0005722stu@sdst.org>
2020-02-18 22:06:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+x = 10
+print('welcome to springfield plus!')
+print('choose an alias')
+alias = input()
+print('''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+''')
+def sendmsg():
+  msg = input()
+  print('[' + alias + '] ' + msg)
+while x == 10:
+  sendmsg()
